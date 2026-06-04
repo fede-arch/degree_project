29c7a0680732ae3c48f8efa4297563a90b5ec986
fede-arch <dangelo.2056804@studenti.uniroma1.it>
2026-06-04 04:00:10 +0200

    Restore description_generator.py and model.sdf.in, fix run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 xhost +local:docker
 
+python3 lrauv_description/scripts/description_generator.py \
+  lrauv_description/models/tethys/model.sdf.in \
+  lrauv_description/models/tethys/model.sdf
 
 docker run -it --rm \
   --env DISPLAY=$DISPLAY \
@@ -8,9 +11,11 @@ docker run -it --rm \
   --volume $PWD:/lrauv_ws/src/degree_project \
   lrauv:harmonic \
   bash -c "source /setup.sh && \
-    mkdir -p /tmp/build && cd /tmp/build && \
-    cmake /lrauv_ws/src/degree_project/lrauv_gazebo_plugins -DCMAKE_BUILD_TYPE=Release && \
-    make -j4 HydrodynamicsPlugin && \
+    if [ ! -f /tmp/degree_build/libHydrodynamicsPlugin.so ]; then \
+      mkdir -p /tmp/degree_build && cd /tmp/degree_build && \
+      cmake /lrauv_ws/src/degree_project/lrauv_gazebo_plugins -DCMAKE_BUILD_TYPE=Release && \
+      make -j4 HydrodynamicsPlugin; \
+    fi && \
     export GZ_SIM_RESOURCE_PATH=/lrauv_ws/src/degree_project/lrauv_description/models && \
-    export GZ_SIM_SYSTEM_PLUGIN_PATH=/tmp/build && \
+    export GZ_SIM_SYSTEM_PLUGIN_PATH=/tmp/degree_build && \
     gz sim /lrauv_ws/src/degree_project/lrauv_gazebo_plugins/worlds/tethys_at_empty_environment.sdf"
