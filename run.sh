b5647e1736131afacdcc53bfae6190d2e5b2c84b
fede-arch <dangelo.2056804@studenti.uniroma1.it>
2026-06-04 04:17:51 +0200

    Improve run.sh: build cache, suppress warnings, add progress messages

--- run.sh
+++ run.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
-xhost +local:docker
+xhost +local:docker 2>/dev/null
 
+echo "Generating model.sdf..."
 python3 lrauv_description/scripts/description_generator.py \
   lrauv_description/models/tethys/model.sdf.in \
   lrauv_description/models/tethys/model.sdf
 
 docker run -it --rm \
   --env DISPLAY=$DISPLAY \
+  --env XDG_RUNTIME_DIR=/tmp/runtime-developer \
   --volume /tmp/.X11-unix:/tmp/.X11-unix \
   --volume $PWD:/lrauv_ws/src/degree_project \
   lrauv:harmonic \
-  bash -c "source /setup.sh && \
-    if [ ! -f /tmp/degree_build/libHydrodynamicsPlugin.so ]; then \
-      mkdir -p /tmp/degree_build && cd /tmp/degree_build && \
-      cmake /lrauv_ws/src/degree_project/lrauv_gazebo_plugins -DCMAKE_BUILD_TYPE=Release && \
-      make -j4 HydrodynamicsPlugin; \
+  bash -c "source /setup.sh 2>/dev/null && \
+    if [ ! -f /lrauv_ws/src/degree_project/docker_build/libHydrodynamicsPlugin.so ]; then \
+      echo 'Building HydrodynamicsPlugin...' && \
+      mkdir -p /lrauv_ws/src/degree_project/docker_build && \
+      cd /lrauv_ws/src/degree_project/docker_build && \
+      cmake ../lrauv_gazebo_plugins -DCMAKE_BUILD_TYPE=Release -Wno-dev 2>/dev/null && \
+      make -j4 HydrodynamicsPlugin 2>/dev/null && \
+      echo 'Plugin built successfully!'; \
+    else \
+      echo 'Plugin already built'; \
     fi && \
+    echo 'Launching Gazebo...' && \
     export GZ_SIM_RESOURCE_PATH=/lrauv_ws/src/degree_project/lrauv_description/models && \
-    export GZ_SIM_SYSTEM_PLUGIN_PATH=/tmp/degree_build && \
-    gz sim /lrauv_ws/src/degree_project/lrauv_gazebo_plugins/worlds/tethys_at_empty_environment.sdf"
+    export GZ_SIM_SYSTEM_PLUGIN_PATH=/lrauv_ws/src/degree_project/docker_build && \
+    gz sim /lrauv_ws/src/degree_project/lrauv_gazebo_plugins/worlds/tethys_at_empty_environment.sdf 2>/dev/null"
