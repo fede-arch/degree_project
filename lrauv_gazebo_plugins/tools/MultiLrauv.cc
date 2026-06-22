408fda5b135c1ad12f98e13c31a7d7bbe8393427
fede-arch <dangelo.2056804@studenti.uniroma1.it>
2026-06-22 23:32:25 +0200

    BASE clean up MultiLrauv

--- lrauv_gazebo_plugins/tools/MultiLrauv.cc
+++ lrauv_gazebo_plugins/tools/MultiLrauv.cc
@@ -5,21 +5,26 @@
 #include <condition_variable>
 #include <gz/transport/Node.hh>
 #include <gz/msgs/stringmsg.pb.h>
-
-static std::mutex              g_mtx;
-static std::condition_variable g_cv;
-static int                     g_n_drones = 0;
-static int                     g_n_done   = 0;
+using namespace std;
 
 struct DroneResult {
-    std::string tag  = "";
+    string tag  = "";
     bool        done = false;
 };
 
-static std::vector<DroneResult> g_results;
-static void parse_result(const std::string &data, DroneResult &r) {
-    for (const auto &tag : {"ARRIVED", "COLLISION", "TIMEOUT"}) {
-        if (data.find(tag) != std::string::npos) {
+struct EpisodeSession {
+    mutex               mtx;
+    condition_variable  cv;
+    int                 n_drones = 0;
+    int                 n_done   = 0;
+    vector<DroneResult> results;
+};
+
+static EpisodeSession g_session; 
+
+static void parse_result(const string &data, DroneResult &r) {
+    for (const auto &tag : {"arrived", "collision", "timeout"}) {
+        if (data.find(tag) != string::npos) {
             r.tag = tag;
             return;
         }
@@ -30,52 +35,52 @@ static void parse_result(const std::string &data, DroneResult &r) {
 // MAIN
 int main(int argc, char **argv) {
     if (argc < 2) {
-        std::cerr << "Usage: MultiLrauv <n_drones>" << std::endl;
+        cerr << "Usage: MultiLrauv <n_drones>" << endl;
         return 1;
     }
 
-    g_n_drones = std::stoi(argv[1]);
-    g_results.resize(g_n_drones);
+    g_session.n_drones = stoi(argv[1]);
+    g_session.results.resize(g_session.n_drones);
 
     gz::transport::Node node;
 
-    for (int i = 0; i < g_n_drones; i++) {
-        std::string topic = "/tethys_" + std::to_string(i) + "/es/episode_result";
-        std::cout << "[MULTI] Listening on " << topic << std::endl;
+    for (int i = 0; i < g_session.n_drones; i++) {
+        string topic = "/tethys_" + to_string(i) + "/es/episode_result";
+        cout << "[MULTI] Listening on " << topic << endl;
 
         node.Subscribe<gz::msgs::StringMsg>(topic,
             [i](const gz::msgs::StringMsg &msg) {
-                std::lock_guard<std::mutex> lock(g_mtx);
-                if (g_results[i].done) return;
-                parse_result(msg.data(), g_results[i]);
-                g_results[i].done = true;
-                g_n_done++;
-                std::cout << "[MULTI] drone_" << i
-                          << " -> " << g_results[i].tag << std::endl;
-                if (g_n_done == g_n_drones)
-                    g_cv.notify_all();
+                lock_guard<mutex> lock(g_session.mtx);
+                if (g_session.results[i].done) return;
+                parse_result(msg.data(), g_session.results[i]);
+                g_session.results[i].done = true;
+                g_session.n_done++;
+                cout << "[MULTI] drone_" << i
+                          << " -> " << g_session.results[i].tag << endl;
+                if (g_session.n_done == g_session.n_drones)
+                    g_session.cv.notify_all();
             });
     }
 
-    std::cout << "[MULTI] Waiting for " << g_n_drones << " drones..." << std::endl;
+    cout << "[MULTI] Waiting for " << g_session.n_drones << " drones..." << endl;
 
-    std::unique_lock<std::mutex> lock(g_mtx);
-    g_cv.wait(lock, [] { return g_n_done == g_n_drones; });
+    unique_lock<mutex> lock(g_session.mtx);
+    g_session.cv.wait(lock, [] { return g_session.n_done == g_session.n_drones; });
 
     int arrived = 0, collision = 0, timeout = 0;
-    for (auto &r : g_results) {
-        if      (r.tag == "ARRIVED")   arrived++;
-        else if (r.tag == "COLLISION") collision++;
+    for (auto &r : g_session.results) {
+        if      (r.tag == "arrived")   arrived++;
+        else if (r.tag == "collision") collision++;
         else                           timeout++;
     }
 
-    std::cout << "\n========== RISULTATI ==========" << std::endl;
-    std::cout << "Droni totali : " << g_n_drones << std::endl;
-    std::cout << "ARRIVED      : " << arrived    << std::endl;
-    std::cout << "COLLISION    : " << collision  << std::endl;
-    std::cout << "TIMEOUT      : " << timeout    << std::endl;
-    std::cout << "Success rate : "
-              << (100.0 * arrived / g_n_drones) << "%" << std::endl;
+    cout << "\n========== RISULTATI ==========" << endl;
+    cout << "Droni totali : " << g_session.n_drones << endl;
+    cout << "ARRIVED      : " << arrived    << endl;
+    cout << "COLLISION    : " << collision  << endl;
+    cout << "TIMEOUT      : " << timeout    << endl;
+    cout << "Success rate : "
+              << (100.0 * arrived / g_session.n_drones) << "%" << endl;
 
     return 0;
 }
